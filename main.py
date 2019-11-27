a49835297779059345d47d3612b9b11a01578e0f
patrickkim8 <55516266+patrickkim8@users.noreply.github.com>
2019-11-27 00:38:54 -0500

    mean value

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-
+from typing import List
 
 def main():
     assert len(sys.argv) > 1, "No input file path specified."
@@ -8,6 +8,19 @@ def main():
     print(f"Processing input file: {input_file_path}")
 
     # TODO: Fill in the actual logic here!
+    with open(sys.argv[1]) as s:
+        for line in s:
+            value = line.split(",")
+
+            y = 0
+            for i in value:
+                x = float(i)
+                y = x + y
+                z = y / len(value)
+            print(f"{z}")
+
+
+
 
 
 if __name__ == "__main__":
